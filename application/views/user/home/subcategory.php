4352e8ad41531733ca2c22328b87ab23369ede3f
Aadil <aadil.svap@gmail.com>
2020-07-31 15:05:54 +0530

    changes in subcategory

--- application/views/user/home/subcategory.php
+++ application/views/user/home/subcategory.php
@@ -1,25 +1,24 @@
 <div class="breadcrumb-bar">
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col">
-					<div class="breadcrumb-title">
-						<h2>Subcategory</h2>
-					</div>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<div class="breadcrumb-title">
+					<h2>Subcategories</h2>
 				</div>
-				<div class="col-auto float-right ml-auto breadcrumb-menu">
-					<nav aria-label="breadcrumb" class="page-breadcrumb">
-						<ol class="breadcrumb">
-							<li class="breadcrumb-item"><a href="<?php echo base_url();?>">Home</a></li>
-							<li class="breadcrumb-item active" aria-current="page">Subcategory</li>
-						</ol>
-					</nav>
-				</div>
+			</div>
+			<div class="col-auto float-right ml-auto breadcrumb-menu">
+				<nav aria-label="breadcrumb" class="page-breadcrumb">
+					<ol class="breadcrumb">
+						<li class="breadcrumb-item"><a href="<?php echo base_url();?>">Home</a></li>
+						<li class="breadcrumb-item active" aria-current="page">Subcategories</li>
+					</ol>
+				</nav>
 			</div>
 		</div>
 	</div>
-	<div class="content">
+</div>
+<div class="content">
 	<div class="container">
-		<p> <b><?php  echo count($subcategory); ?> Subcategories Found</b></p>
 		<div class="">
 			<?php 
 			$pagination=explode('|',$this->ajax_pagination->create_links());
@@ -27,36 +26,38 @@
 		</div>					
 		<div class="catsec">
 			<div class="row" id="dataList">
-<?php
 
-	  if($subcategory){
-	  foreach($subcategory as $subcategories) { ?>
-	<div class="col-lg-4 col-md-6">
-		<div class="service-widget">
-			<div class="service-img">
-				<a href="<?php echo base_url().'services/'.$subcategories['id']; ?>">
-					<img class="img-fluid serv-img" alt="Service Image" src="<?php echo base_url().$subcategories['subcategory_image']; ?>">
+			<?php
+			if(!empty($subcategory)) {
+				foreach ($subcategory as $subcategories) {
+					$category_name=strtolower($subcategories['subcategory_name']);
+				?>
+			<div class="col-lg-4 col-md-6">
+				<a href="<?php echo base_url();?>services/<?php echo $subcategories['id']; ?>">
+					<div class="cate-widget">
+						<img src="<?php echo base_url().$subcategories['subcategory_image'];?>" alt="">
+						<div class="cate-title">
+							<h3><span><i class="fas fa-circle"></i> <?php echo ucfirst($subcategories['subcategory_name']);?></span></h3>
+						</div>
+						<div class="cate-count">
+							<i class="fas fa-clone"></i> <?php//echo $subcategories['category_count'];?>
+						</div>
+					</div>
 				</a>
 			</div>
-			<div class="service-content">
-				<h3 class="title">
-					<a href="<?php echo base_url().'services/'.$subcategories['id']; ?>"><?php echo $subcategories['subcategory_name']; ?></a>
-				</h3>
+			<?php } }
+			else { 
+
+			echo '<div class="col-lg-12">
+			<div class="category">
+			No Categories Found
 			</div>
-		</div>
+			</div>';
+			} 
 
-	</div>
-<?php }} 
-else { 
-
-	echo '<div class="col-lg-12">
-		<p class="mb-0">
-			No Services Found
-		</p>
-	</div>';
-	 }  ?>
+			echo $this->ajax_pagination->create_links();
+			?>
 			</div>
 		</div>
 	</div>
 </div>
-
